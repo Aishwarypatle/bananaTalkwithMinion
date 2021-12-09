7b18041f606cd8569c17032f58eb81c9d05bec88
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2021-12-09 21:15:52 +0530

    translated nd cleaned code with proper indentation

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ var btnTranslate= document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output");
 
+
+
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
-function clickEventHandler() 
-{
-    outputDiv.innerText = "shdshdk "+ txtInput.value;
-};
+
+
+
+    function getTranslateUrl(text)
+        {
+        return serverURL+ "?"+ "text="+text
+        }
+
+    function errorHandler(error)
+        {
+    console.log("Error", error);
+    alert("Server not working ! Try after some time ")
+        }
+
+    function clickHandler() 
+        {
+        var inputText = txtInput.value;
+    fetch(getTranslateUrl(inputText))
+    .then(response => response.json())
+    .then(json =>   {
+                    var translatedText = json.contents.translated;
+                    outputDiv.innerText = translatedText;
+                    })
+    .catch(errorHandler)
+        };
 
 btnTranslate.addEventListener("click", clickEventHandler) 
