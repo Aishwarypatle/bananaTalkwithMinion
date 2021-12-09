151e06456f991811be1f2a0b7f83447d26a97f05
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2021-12-09 20:38:42 +0530

    adding butuon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("I am aishwary")
+console.log("script");
+var btnTranslate= document.querySelector("#btn-translate");
+
+var username = prompt("Give me your username..");
+var welcomeMessage = username+ " is your username";
+alert(welcomeMessage); 
+function clickEventHandler() {
+    console.log("Clickeddd");}
+
+btnTranslate.addEventListener("click", clickEventHandler()) 
